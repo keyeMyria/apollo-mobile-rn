b53fa20a3ac3c79ba1d2255958dd311c15019f0b
DESKTOP-TOCKQL7\gizem <gizemkorkmaz219@gmail.com>
2018-06-08 15:52:16 +0300

    bestoftab da kriter seçimi düzenlendi

--- apolloMobileRn/src/pages/bestOfTabPages/BestShopsPage.js
+++ apolloMobileRn/src/pages/bestOfTabPages/BestShopsPage.js
@@ -4,13 +4,60 @@ import { connect } from 'react-redux';
 import { getPing, fetchReportTop } from 'apollo-rn-redux-helper/src/actions';
 import LinearGradient from 'react-native-linear-gradient';
 import { localizedText } from './../../helpers/Localization/Localization';
+import BarChart from './../../components/charts/BarChart';
+import { widthPercentageToDP } from '../../helpers';
+import OrderCriteria from './bestOfTabPagesComponents/OrderCriteria';
+const shops = [
+	{
+		text: 'Teknosa',
+		value: 100
+	},
+	{
+		text: 'big',
+		value: 75
+	},
+	{
+		text: 'English Home',
+		value: 60
+	},
+	{
+		text: 'Macrocenter',
+		value: 40
+	},
+	{
+		text: 'MADO',
+		value: 29
+	},
+	{
+		text: 'Atasun Optik',
+		value: 10
+	},
+	{
+		text: 'başkent doğangaz ',
+		value: 7
+	},
+	{
+		text: 'PEPERONCINO',
+		value: 2
+	},
+	{
+		text: 'a1066',
+		value: 1
+	}
+];
 
 class BestShopsPage extends Component {
 	render() {
 		console.log('best shops: ', localizedText.STORE);
 		return (
 			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				<Text>Best shops page</Text>
+				<OrderCriteria />
+
+				<BarChart
+					data={shops}
+					chartWidth={widthPercentageToDP('80%')}
+					chartHeight={widthPercentageToDP('80%')}
+				/>
 			</View>
 		);
 	}
@@ -21,15 +68,15 @@ var styles = StyleSheet.create({
 		paddingLeft: 15,
 		paddingRight: 15,
 		borderRadius: 5
-	},
-	buttonText: {
-		fontSize: 18,
-		fontFamily: 'Gill Sans',
-		textAlign: 'center',
-		margin: 10,
-		color: '#ffffff',
-		backgroundColor: 'transparent'
 	}
+	// buttonText: {
+	// 	fontSize: 18,
+	// 	fontFamily: 'Gill Sans',
+	// 	textAlign: 'center',
+	// 	margin: 10,
+	// 	color: '#ffffff',
+	// 	backgroundColor: 'transparent'
+	// }
 });
 
 const mapStateToProps = state => {
---
+++ apolloMobileRn/src/pages/bestOfTabPages/bestOfTabPagesComponents/Card.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { widthPercentageToDP, Colors } from '../../../helpers';
+class Card extends Component {
+	render() {
+		return <View />;
+	}
+}
+export default Card;
